452b9398444f726e7194d34e1c9c54b7644e245a
zaid1912 <zaidmuzammil1912002@gmail.com>
2025-01-21 23:53:53 +0500

    jobs recommendations page done

--- packages/Arthur/src/index.ts
+++ packages/Arthur/src/index.ts
@@ -4,10 +4,22 @@ import dotenv from 'dotenv';
 import connectToDatabase from './utils/database';
 import router from './routes/resumeRoutes'; // Updated import
 import { PORT } from './config';
+import cors from 'cors';
 
 dotenv.config();
 
 const app: Application = express();
+
+// cors
+app.use(cors(
+  {
+    origin: 'http://localhost:3000',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    credentials: true,
+    allowedHeaders: 'Content-Type,Authorization',
+  }
+));
+
 
 // Middleware
 app.use(bodyParser.json());
