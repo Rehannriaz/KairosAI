2f8cf499c99ecd1cc5a7a696db48144d32a2282b
Rehannriaz <u2021438@giki.edu.pk>
2025-01-22 15:18:36 +0500

    color and hydration fix

--- packages/Ada/src/components/global/Header.tsx
+++ packages/Ada/src/components/global/Header.tsx
@@ -9,12 +9,28 @@ import NotificationsIcon from '@mui/icons-material/Notifications';
 import MailIcon from '@mui/icons-material/Mail';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { useSidebar } from '@/contexts/SidebarContext';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import authServiceInstance from '@/api/authService';
+import { useRouter } from 'next/navigation';
 
 const Header = () => {
   const { toggleSidebar } = useSidebar(); // Use the context
+  const router = useRouter();
+  const handleLogout = async () => {
+    try {
+      await authServiceInstance.logout();
+      router.refresh();
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
-    <header className="flex items-center justify-between px-4 py-6 bg-white">
+    <div className="flex items-center justify-between px-4 py-6 bg-white">
       {/* Logo and Hamburger */}
       <div className="flex items-center space-x-4">
         {/* Logo */}
@@ -46,15 +62,35 @@ const Header = () => {
         <MailIcon className="text-xl cursor-pointer" />
         <FavoriteIcon className="text-xl cursor-pointer" />
         {/* Profile Picture */}
-        <Image
-          src="/next.svg"
-          alt="Profile"
-          width={40}
-          height={40}
-          className="rounded-full cursor-pointer"
-        />
+
+        <Popover>
+          <PopoverTrigger>
+            {' '}
+            <Image
+              src="/next.svg"
+              alt="Profile"
+              width={40}
+              height={40}
+              className="rounded-full cursor-pointer"
+            />
+          </PopoverTrigger>
+          <PopoverContent className="">
+            <div className="flex flex-col ml-2">
+              <span className="font-bold">username</span>
+              <div className="text-right cursor-pointer mt-5 p-2 w-full rounded-r-md hover:bg-[#222222] hover:text-white transition duration-100 ease-in">
+                View Profile
+              </div>
+              <div
+                onClick={handleLogout}
+                className="text-right cursor-pointer p-2 mt-5 w-full rounded-r-md hover:bg-[#222222] hover:text-white transition duration-100 ease-in"
+              >
+                Log Out
+              </div>
+            </div>
+          </PopoverContent>
+        </Popover>
       </div>
-    </header>
+    </div>
   );
 };
 
