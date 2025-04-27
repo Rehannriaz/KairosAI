77b51c825ba5ea472ef788dffc2cc3c073ad4dc6
Rehan <rehanriaz1221@gmail.com>
2025-04-27 22:09:02 +0500

    test commit

--- packages/Ada/src/config/index.ts
+++ packages/Ada/src/config/index.ts
@@ -2,3 +2,5 @@ export const authBaseURL = process.env.NEXT_PUBLIC_AUTH_BASE_URL;
 export const johnBaseURL = process.env.NEXT_PUBLIC_JOHN_BASE_URL;
 export const pollyBaseURL = process.env.NEXT_PUBLIC_POLLY_BASE_URL;
 export const arthurBaseURL = process.env.NEXT_PUBLIC_ARTHUR_BASE_URL;
+
+// urls
